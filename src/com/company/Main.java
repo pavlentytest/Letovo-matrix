e2df16dc963c42b2b0fae52bc9226c47c973d9fa
Pavel <impetus@yandex.ru>
2020-10-07 17:39:23 +0300

    Add comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // добавил комментарий
+        
        /* int[] a;
         a = new int[5];
         for(int i=0;i<5;i++) {
